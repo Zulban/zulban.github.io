32340727c905156f16586aff0f6dbc7e456f8d68
Stuart Spence <stuart.a.spence@gmail.com>
2021-07-30 17:34:02 -0400

    added CNAME and soft link to freeze cli

--- cli.py
+++ cli.py
@@ -17,10 +17,18 @@ Options:
     -v --version          Show version.
 """
 
+import os
 from app import create_app
 from utilities import docopt
 from constants import PROJECT_ROOT
 from flask_frozen import Freezer
+
+def create_static_doc_files(folder):
+    cname=folder+"CNAME"
+    with open(cname,"w") as f:
+        f.write("www.meteohack.ca")
+    
+    os.symlink("home.html",folder+"index.html")
 
 def main(args):
     
@@ -34,11 +42,12 @@ def main(args):
     
     elif args["freeze"]:
         app.config["FREEZER_IGNORE_MIMETYPE_WARNINGS"]=True
-        output=PROJECT_ROOT+"/"+args["--freeze"]
+        output=PROJECT_ROOT+"/"+args["--freeze"]+"/"
         app.config["FREEZER_DESTINATION"]=output
         freezer = Freezer(app)
         freezer.freeze()
         print("Froze Flask app: "+output)
+        create_static_doc_files(output)
     
     print("Done.")
 
